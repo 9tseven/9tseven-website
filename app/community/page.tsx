bf97da5637c7b03e77fa32768ac83f7bdb2b845c
maen0002 <maen0002@stud.kea.dk>
2026-04-20 15:01:24 +0200

    feat: add community section page

---
+++ app/community/page.tsx
@@ -0,0 +1,12 @@
+import AsciiRun from "../components/CommunitySection/AsciiRun";
+
+export default function Community() {
+  return (
+    <main className="p-20 ">
+      <div className="flex items-center place-content-between">
+        <AsciiRun></AsciiRun>
+        <h1 className="text-5xl font-bold leading-none tracking-tight text-black md:text-6xl lg:text-[10rem]">Community</h1>
+      </div>
+    </main>
+  );
+}
