3aac4a19892f8ecd5d374e5645fcd9cafc11bde8
buchhavee <mach0015@stud.kea.dk>
2026-04-26 21:51:17 +0200

    Revert "feat(products): query compareAtPrice, quantityAvailable, and price per variant"
    This reverts commit 1fdc54405cda54561e1f7c69462a39dd24ecc148.

--- app/lib/queries/products.ts
+++ app/lib/queries/products.ts
@@ -32,13 +32,6 @@ const PRODUCT_FIELDS = `
       node {
         id
         availableForSale
-        quantityAvailable
-        price {
-          amount
-        }
-        compareAtPrice {
-          amount
-        }
         selectedOptions {
           name
           value
