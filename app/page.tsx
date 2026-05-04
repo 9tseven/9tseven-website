b4ba03ce8539c0bd6f48c79d123ebe9ab1232496
buchhavee <mach0015@stud.kea.dk>
2026-05-04 21:49:41 +0200

    chore: organize public assets and remove unused import
    - Move public/{Hero,Logo,Manifest,PhotoSection,CommunitySection,ProductPlaceholder} under public/(images)/
    - Drop unused InspoSection import from app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-// app/page.tsx
 import HeroSection from "./components/HeroSection";
 import MantraSection from "./components/MantraSection";
 import FeaturedProductsSection from "./components/FeaturedProductsSection";
 import HomeImageSection from "./components/HomeImageSection";
 import BrandStatementSection from "./components/BrandStatementSection";
 import BlogSection from "./components/BlogSection";
-import InspoSection from "./components/InspoSection";
 
 export default function Home() {
   return (
